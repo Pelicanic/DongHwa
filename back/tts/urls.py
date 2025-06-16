b39b694ff405449fef375865e5cf2df81aa2d29e
shlee <blksw90@gmail.com>
2025-06-16 15:58:55 +0900

    간단 TTS (AI_question) 읽기 기능(스트리밍 방식) 추가.

--- back/tts/urls.py
+++ back/tts/urls.py
@@ -1,7 +1,8 @@
 # back/tts/urls.py
 from django.urls import path
-from .views import generate_story_audio
+from .views import generate_story_audio, stream_qa_audio
 
 urlpatterns = [
     path("generate/", generate_story_audio, name="generate_story_audio"),
+    path("qa-audio/<int:qa_id>/", stream_qa_audio, name="stream-qa-audio"),
 ]
